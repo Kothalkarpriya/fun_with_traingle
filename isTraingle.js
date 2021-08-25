ae2e82abee94e07c4d4efb04e58a202b8b3ad6ec
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-08-25 22:45:05 +0530

    quiz is initialised

--- isTraingle.js
+++ isTraingle.js
@@ -1,24 +1,24 @@
-const angleInput = document.querySelectorAll('.angle-input'); 
+const angleInput = document.querySelectorAll('.angle-input');
 const buttonIsTraingle = document.querySelector('#btn-is-traingle');
 const outPut = document.querySelector('#output');
 
-buttonIsTraingle.addEventListener("click",isTraingle);
+buttonIsTraingle.addEventListener("click", isTraingle);
 
 
-function isTraingle(){
+function isTraingle() {
     const checkSum = calculateSum(Number(angleInput[0].value), Number(angleInput[1].value), Number(angleInput[2].value));
     // console.log(checkSum);
-    if(checkSum == 180){
+    if (checkSum == 180) {
         console.log("Yeah! It forms a Traingle");
         outPut.innerText = "Yeah! It forms a Traingle";
-    }else{
+    } else {
         console.log("Oh! It doesn't form a Traingle");
         outPut.innerText = "Oh! It doesn't form a Traingle";
     }
 
 }
 
-function calculateSum(angle1, angle2, angle3){
+function calculateSum(angle1, angle2, angle3) {
 
     const sum = angle1 + angle2 + angle3;
     // console.log(sum)
