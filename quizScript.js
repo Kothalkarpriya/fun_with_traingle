dbf6272f892a37f2abf020b526b593988bc1182c
Priya Kothalkar <priyakothalkar2973@gmail.com>
2021-08-26 21:59:44 +0530

    update javascript logic

---
+++ quizScript.js
@@ -0,0 +1,23 @@
+const quizForm = document.querySelector('.quiz-form');
+const submitAnswer = document.querySelector('#sub-ans');
+const output = document.querySelector('#output');
+
+const correctAnswer = ['90°', 'right'];
+
+submitAnswer.addEventListener('click', calculateScore);
+
+function calculateScore() {
+    let score = 0;
+    let index = 0;
+    const formData = new FormData(quizForm);
+    for (let value of formData.values()) {
+        // console.log(value);
+        if (value === correctAnswer[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    // console.log(formData)
+    // console.log(score);
+    output.innerText = 'You score is : ' + score;
+}
